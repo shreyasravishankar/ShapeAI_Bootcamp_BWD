9ee81eee388091c54ab5919ceb5a9dec3aba574c
shreyasravishankar <85414437+shreyasravishankar@users.noreply.github.com>
2021-06-05 23:56:51 +0400

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Copyrights to Shreyas @ 2021</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This was an amazing bootcamp taken up by Mr.Shaurya Sinha. We covered
+        everything from Scratch including Javascript React.js, HTML and I am
+        very grateful that I picked up Shape AI. It was a very great Bootcamp.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
